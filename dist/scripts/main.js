ddc6fbca0ec13cd38006b9cf7da28473af4a0238
Kasador <huntershaw23@gmail.com>
2019-04-30 16:42:49 -0600

    ADDED: fade-in effect on scroll of content.

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -501,3 +501,19 @@ function otherLangHover() {
         console.log('mouse out');
     });
 }
+// fade in on scroll 
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hideme').each( function(){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ) {
+                $(this).animate({'opacity':'1'},700);   
+            } 
+        }); 
+    });
+});
